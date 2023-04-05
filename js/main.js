567c6a09575a1807d7f5c2ca8168cc72cf2a280f
dgobster <gobster@wisc.edu>
2023-04-04 23:17:12 -0500

    activity 9 partial

--- js/main.js
+++ js/main.js
@@ -9,52 +9,57 @@ function setMap() {
         height = 460;
 
     //create new svg container for the map
-    var map = d3.select("body")
+    var map = d3
+        .select("body")
         .append("svg")
         .attr("class", "map")
         .attr("width", width)
         .attr("height", height);
 
     //create Albers equal area conic projection centered on WI
-    var projection = d3.geoAlbers()
+    var projection = d3
+        .geoAlbers()
         .center([0, 43])
         .rotate([89.5, 0, 0])
         .parallels([43, 43])
         .scale(3500)
         .translate([width / 2, height / 2]);
 
-    var path = d3.geoPath()
-        .projection(projection);
+    var path = d3.geoPath().projection(projection);
 
     //use Promise.all to parallelize asynchronous data loading
     var promises = [
         d3.csv("data/nip_plandistrict_20.csv"),
         d3.json("data/nip_plandistrict_2020.topojson"),
+        d3.json("data/Dane_Municipal_Boundaries_2021.topojson"),
     ];
     Promise.all(promises).then(callback);
+
+    function callback(data) {
+        var csvData = data[0],
+            county = data[1],
+            districts = data[2];
+
+        //translate districts, county to TopoJSON
+        var countyDane = topojson.feature(county, county.objects.Dane_Municipal_Boundaries_2021),
+            districtsMadison = topojson.feature(districts, districts.objects.nip_plandistrict_2020).features;
+
+        //add Dane County to map
+        var dane = map
+          .append("path")
+          .datum(countyDane)
+          .attr("class", "dane")
+          .attr("d", path);
+
+        //add madison districts to map
+        var madison = map
+            .selectAll(".madison")
+            .data(districtsMadison)
+            .enter()
+            .append("path")
+            .attr("class", function (d) {
+                return "madison " + d.properties.geo_key;
+            })
+            .attr("d", path);
+    };
 };
-function callback(data) {
-    csvData = data[0],
-        districts = data[1];
-
-    //translate districts TopoJSON
-    var madisonDistricts = topojson.feature(districts, districts.objects.nip_plandistrict_2020);
-
-    //add Europe countries to map --> add & edit if/when add "basemap" regions
-    //var countries = map.append("path")
-        //.datum(europeCountries)
-        //.attr("class", "countries")
-        //.attr("d", path);
-
-    //add France regions to map
-    var madison = map
-        .selectAll(".madison")
-        .data(madisonDistricts)
-        .enter()
-        .append("path")
-        .attr("class", function (d) {
-            return "madison " + d.properties.geo_key;
-        })
-        .attr("d", path);
-};
-
