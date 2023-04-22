c651cb11df8bf29806ba9d27cca291d85dd82b6b
dgobster <gobster@wisc.edu>
2023-04-21 19:54:17 -0500

    Lab 2 partial

--- js/main.js
+++ js/main.js
@@ -2,9 +2,24 @@
 (function () {
 
     //pseudo-global variables
-    var attrArray = ["tot_pop_10", "fam_pov", "gds_svcs", "pc_trst", "trst_trps", "pc_ownvh"]; //list of attributes
+    var attrArray = ["own_occ", "white", "fam_child", "transit_stop", "over_64"]; //list of attributes
     var expressed = attrArray[0]; //initial attribute
 
+    //chart frame dimensions
+    var chartWidth = window.innerWidth * 0.425,
+        chartHeight = 473,
+        leftPadding = 25,
+        rightPadding = 2,
+        topBottomPadding = 5,
+        chartInnerWidth = chartWidth - leftPadding - rightPadding,
+        chartInnerHeight = chartHeight - topBottomPadding * 2,
+        translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
+
+    //create a scale to size bars proportionally to frame and for axis
+    var yScale = d3
+        .scaleLinear()
+        .range([463, 0])
+        .domain([0, 100]);
     //begin script when window loads
     window.onload = setMap();
 
@@ -30,7 +45,7 @@
             .center([0, 43.084])
             .rotate([89.4, 0, 0])
             .parallels([43, 43])
-            .scale(180000)
+            .scale(100000)
             .translate([width / 2, height / 2]);
 
         var path = d3.geoPath().projection(projection);
@@ -72,7 +87,6 @@
             //join csv data to GeoJSON enumeration units
             districtsMadison = joinData(districtsMadison, csvData);
 
-
             //create the color scale
             var colorScale = makeColorScale(csvData);
 
@@ -80,6 +94,8 @@
             setEnumerationUnits(districtsMadison, map, path, colorScale);
             //add coordinated visualization to the map
             setChart(csvData, colorScale);
+            //add dropdown
+            createDropdown(csvData);
 
         };
     }; //end of setMap()
@@ -90,8 +106,8 @@
 
         //loop through csv to assign each set of csv attribute values to geojson region
         for (var i = 0; i < csvData.length; i++) {
-            var csvRegion = csvData[i]; //the current region
-            var csvKey = csvRegion.geo_key; //the CSV primary key
+            var csvMadison = csvData[i]; //the current region
+            var csvKey = csvMadison.geo_key; //the CSV primary key
 
             //loop through geojson regions to find correct region
             for (var a = 0; a < districtsMadison.length; a++) {
@@ -104,7 +120,7 @@
 
                     //assign all attributes and values
                     attrArray.forEach(function (attr) {
-                        var val = parseFloat(csvRegion[attr]); //get csv attribute value
+                        var val = parseFloat(csvMadison[attr]); //get csv attribute value
                         geojsonProps[attr] = val; //assign attribute and value to geojson properties
                     });
                 };
@@ -127,17 +143,15 @@
         var colorScale = d3.scaleQuantile()
             .range(colorClasses);
 
-        //create color scale generator
-        var colorScale = d3.scaleQuantile()
-            .range(colorClasses);
+        //build array of all values of the expressed attribute
+        var domainArray = [];
+        for (var i = 0; i < data.length; i++) {
+            var val = parseFloat(data[i][expressed]);
+            domainArray.push(val);
+        };
 
-        //build two-value array of minimum and maximum expressed attribute values
-        var minmax = [
-            d3.min(data, function (d) { return parseFloat(d[expressed]); }),
-            d3.max(data, function (d) { return parseFloat(d[expressed]); })
-        ];
-        //assign two-value array as scale domain
-        colorScale.domain(minmax);
+        //assign array of expressed values as scale domain
+        colorScale.domain(domainArray);
 
         return colorScale;
     };
@@ -158,92 +172,181 @@
             .style("fill", function (d) {
                 var value = d.properties[expressed];
                 if (value) {
-                    return colorScale(d.properties[expressed]);
+                    return colorScale(value);
+                } else {
+                    return "#ccc";
+                }
+            })
+            .on("mouseover", function (event, d) {
+                highlight(d.properties);
+            })
+    };
+
+    //function to create coordinated bar chart
+    function setChart(csvData, colorScale) {
+
+        //create a second svg element to hold the bar chart
+        var chart = d3
+            .select("body")
+            .append("svg")
+            .attr("width", chartWidth)
+            .attr("height", chartHeight)
+            .attr("class", "chart");
+
+        //create a rectangle for chart background fill
+        var chartBackground = chart
+            .append("rect")
+            .attr("class", "chartBackground")
+            .attr("width", chartInnerWidth)
+            .attr("height", chartInnerHeight)
+            .attr("transform", translate);
+
+        //set bars for each district
+        var bars = chart
+            .selectAll(".bar")
+            .data(csvData)
+            .enter()
+            .append("rect")
+            .sort(function (a, b) {
+                return b[expressed] - a[expressed];
+            })
+            .attr("class", function (d) {
+                return "bar " + d.geo_key;
+            })
+            .attr("width", chartInnerWidth / csvData.length - 1)
+            .on("mouseover", function (event, d) {
+                highlight(d);
+            });
+
+        //create a text element for the chart title
+        var chartTitle = chart
+            .append("text")
+            .attr("x", 40)
+            .attr("y", 40)
+            .attr("class", "chartTitle");
+
+        updateChart(bars, csvData.length, colorScale);
+
+        //create vertical axis generator
+        var yAxis = d3.axisLeft()
+            .scale(yScale);
+
+        //place axis
+        var axis = chart.append("g")
+            .attr("class", "axis")
+            .attr("transform", translate)
+            .call(yAxis);
+
+        //create frame for chart border
+        var chartFrame = chart.append("rect")
+            .attr("class", "chartFrame")
+            .attr("width", chartInnerWidth)
+            .attr("height", chartInnerHeight)
+            .attr("transform", translate);
+    };
+
+    //function to create a dropdown menu for attribute selection
+    function createDropdown(csvData) {
+        //add select element
+        var dropdown = d3.select("body")
+            .append("select")
+            .attr("class", "dropdown")
+            .on("change", function () {
+                changeAttribute(this.value, csvData);
+            });
+
+        //add initial option
+        var titleOption = dropdown
+            .append("option")
+            .attr("class", "titleOption")
+            .attr("disabled", "true")
+            .text("Select Attribute");
+
+        //add attribute name options
+        var attrOptions = dropdown
+            .selectAll("attrOptions")
+            .data(attrArray)
+            .enter()
+            .append("option")
+            .attr("value", function (d) { 
+                return d;
+             })
+            .text(function (d) { 
+                return d;
+             });
+    };
+    //dropdown change event handler
+    function changeAttribute(attribute, csvData) {
+        //change the expressed attribute
+        expressed = attribute;
+
+        //recreate the color scale
+        var colorScale = makeColorScale(csvData);
+
+        //recolor enumeration units
+        var madison = d3
+            .selectAll(".madison")
+            .transition()
+            .duration(1000)
+            .style("fill", function (d) {
+                var value = d.properties[expressed];
+                if (value) {
+                    return colorScale(value);
                 } else {
                     return "#ccc";
                 }
             });
+
+        //Sort, resize, and recolor bars
+        var bars = d3
+            .selectAll(".bar")
+            //Sort bars
+            .sort(function (a, b) {
+                return b[expressed] - a[expressed];
+            })
+            .transition() //add animation
+            .delay(function (d, i) {
+                return i * 20;
+            })
+            .duration(500);
+
+        updateChart(bars, csvData.length, colorScale);
+    }
+    function updateChart(bars, n, colorScale) {
+        //position bars
+        bars.attr("x", function (d, i) {
+            return i * (chartInnerWidth / n) + leftPadding;
+        })
+            //size/resize bars
+            .attr("height", function (d, i) {
+                return 463 - yScale(parseFloat(d[expressed]));
+            })
+            .attr("y", function (d, i) {
+                return yScale(parseFloat(d[expressed])) + topBottomPadding;
+            })
+            //color/recolor bars
+            .style("fill", function (d) {
+                var value = d[expressed];
+                if (value) {
+                    return colorScale(value);
+                } else {
+                    return "#ccc";
+                }
+            });
+
+        //add text to chart title
+        var chartTitle = d3
+            .select(".chartTitle")
+            .text("Percent Regional " + expressed);
+
     }
 
-   //function to create coordinated bar chart
-function setChart(csvData, colorScale){
-    //chart frame dimensions
-    var chartWidth = window.innerWidth * 0.425,
-        chartHeight = 473,
-        leftPadding = 25,
-        rightPadding = 2,
-        topBottomPadding = 5,
-        chartInnerWidth = chartWidth - leftPadding - rightPadding,
-        chartInnerHeight = chartHeight - topBottomPadding * 2,
-        translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
-
-    //create a second svg element to hold the bar chart
-    var chart = d3.select("body")
-        .append("svg")
-        .attr("width", chartWidth)
-        .attr("height", chartHeight)
-        .attr("class", "chart");
-
-    //create a rectangle for chart background fill
-    var chartBackground = chart.append("rect")
-        .attr("class", "chartBackground")
-        .attr("width", chartInnerWidth)
-        .attr("height", chartInnerHeight)
-        .attr("transform", translate);
-
-    //create a scale to size bars proportionally to frame and for axis
-    var yScale = d3.scaleLinear()
-        .range([463, 0])
-        .domain([0, 100]);
-
-    //set bars for each province
-    var bars = chart.selectAll(".bar")
-        .data(csvData)
-        .enter()
-        .append("rect")
-        .sort(function(a, b){
-            return b[expressed]-a[expressed]
-        })
-        .attr("class", function(d){
-            return "bar " + d.geo_key;
-        })
-        .attr("width", chartInnerWidth / csvData.length - 1)
-        .attr("x", function(d, i){
-            return i * (chartInnerWidth / csvData.length) + leftPadding;
-        })
-        .attr("height", function(d, i){
-            return 463 - yScale(parseFloat(d[expressed]));
-        })
-        .attr("y", function(d, i){
-            return yScale(parseFloat(d[expressed])) + topBottomPadding;
-        })
-        .style("fill", function(d){
-            return colorScale(d[expressed]);
-        });
-
-    //create a text element for the chart title
-    var chartTitle = chart.append("text")
-        .attr("x", 40)
-        .attr("y", 40)
-        .attr("class", "chartTitle")
-        .text("Number of Variable " + expressed[3] + " in each region");
-
-    //create vertical axis generator
-    var yAxis = d3.axisLeft()
-        .scale(yScale);
-
-    //place axis
-    var axis = chart.append("g")
-        .attr("class", "axis")
-        .attr("transform", translate)
-        .call(yAxis);
-
-    //create frame for chart border
-    var chartFrame = chart.append("rect")
-        .attr("class", "chartFrame")
-        .attr("width", chartInnerWidth)
-        .attr("height", chartInnerHeight)
-        .attr("transform", translate);
-};
-
+    //function to highlight enumeration units and bars
+    function highlight(props) {
+        //change stroke
+        var selected = d3
+            .selectAll("." + props.geo_key)
+            .style("stroke", "blue")
+            .style("stroke-width", "2");
+    };
 })();
